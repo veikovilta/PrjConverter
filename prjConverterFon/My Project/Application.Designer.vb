69332f523a485673393ae03500bc8ac585778401
Veiko Vilta <vevilt@ICT-50119K>
2024-01-31 16:57:03 +0200

    first

--- prjConverterFon/My Project/Application.Designer.vb
+++ prjConverterFon/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.prjConverterFon.Form1
+            Me.MainForm = Global.prjConverterFon.FormConverter
         End Sub
     End Class
 End Namespace
